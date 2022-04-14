a9fd8e0c1d59befd9104360dc06b76a3c615ef32
Fangneng Zhan <fzhan@mpi-inf.mpg.de>
2022-04-14 16:17:12 +0200

    Update train_options.py

--- options/train_options.py
+++ options/train_options.py
@@ -25,6 +25,9 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--beta2', type=float, default=0.999, help='momentum term of adam')
         parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate for adam')
         parser.add_argument('--D_steps_per_G', type=int, default=1, help='number of discriminator iterations per generator iterations.')
+        
+        parser.add_argument('--mcl', action='store_true', help='Use marginal contrastive learning')
+        parser.add_argument('--nce_w', type=float, default=0.01, help='weight for marginal contrastive loss')
 
         # for discriminators
         parser.add_argument('--ndf', type=int, default=64, help='# of discrim filters in first conv layer')
